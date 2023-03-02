13817a30634b61f4831e5422180d8c6bf9f143ec
Matias Benary <matiasbenary@gmail.com>
2023-03-02 17:38:44 -0300

    :bug: Fix details

--- tests/Unit/Actions/EntityCreateActionTest.php
+++ tests/Unit/Actions/EntityCreateActionTest.php
@@ -10,7 +10,7 @@ class EntityCreateActionTest extends TestCase
 {
     public function test_create_entity_complete()
     {
-        $entityRequest = EntityCreateData::from(['business_name' => 'Test', 'fantasy_name' => 'fantasy', 'cuit' => 123456789, 'cbu' => 9876543211234, 'email' => 'test@test.com']);
+        $entityRequest = EntityCreateData::from(['business_name' => 'test', 'fantasy_name' => 'fantasy', 'cuit' => 123456789, 'cbu' => 9876543211234, 'email' => 'test@test.com']);
         $createEntityAction = new ActionsEntityCreateAction($entityRequest);
 
         $entity = $createEntityAction->execute();
@@ -26,7 +26,7 @@ class EntityCreateActionTest extends TestCase
     public function test_create_entity_without_cbu()
     {
         $entityRequest = EntityCreateData::from([
-            'business_name' => 'Test', 'fantasy_name' => 'fantasy', 'cuit' => 123456789, 'email' => 'test@test.com',
+            'business_name' => 'test', 'fantasy_name' => 'fantasy', 'cuit' => 123456789, 'email' => 'test@test.com',
         ]);
         $createEntityAction = new ActionsEntityCreateAction($entityRequest);
 

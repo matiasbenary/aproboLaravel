89bb19eaf5c4903af33ebf1dae9709380c8f1d57
Matias Benary <matiasbenary@gmail.com>
2023-03-19 18:50:48 -0300

    :bug: Fix creation entities part 2

--- tests/Feature/Http/Controllers/Auth/RegisterClientControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/RegisterClientControllerTest.php
@@ -17,54 +17,54 @@ class RegisterClientControllerTest extends TestCase
      */
     public function test_successfully_register_the_client()
     {
-        // $response = $this->postJson('api/registerClient', ['name' => 'test testing', 'business_name' => 'empresa de test', 'email' => 'creacionUser@test.com', 'cuit' => 123456789, 'password' => 'password']); // ['name' => 'test testing', 'business_name' => 'empresa de test', 'email' => 'test@test.com', 'cuit' => 123456789, 'password' => "password"]);
+        $response = $this->postJson('api/registerClient', ['name' => 'test testing', 'business_name' => 'empresa de test', 'email' => 'creacionUser@test.com', 'cuit' => 123456789, 'password' => 'password']); // ['name' => 'test testing', 'business_name' => 'empresa de test', 'email' => 'test@test.com', 'cuit' => 123456789, 'password' => "password"]);
 
-        // $response->assertStatus(200)
-        //     ->assertJson(['message' => ['Client successfully registered']])
-        //     ->assertJsonStructure([
-        //         'access_token',
-        //         'token_type',
-        //         'expires_in',
-        //         'entities'
-        //     ]);
-        // $this->assertDatabaseHas('users', [
-        //     'name' => 'test testing',
-        //     'email' => 'creacionUser@test.com',
-        //     'is_root' => 0,
-        // ]);
+        $response->assertStatus(200)
+            ->assertJson(['message' => ['Client successfully registered']])
+            ->assertJsonStructure([
+                'access_token',
+                'token_type',
+                'expires_in',
+                'entities'
+            ]);
+        $this->assertDatabaseHas('users', [
+            'name' => 'test testing',
+            'email' => 'creacionUser@test.com',
+            'is_root' => 0,
+        ]);
 
-        // $this->assertDatabaseHas('entities', [
-        //     'business_name' => 'empresa de test',
-        //     'cuit' => 123456789,
-        // ]);
+        $this->assertDatabaseHas('entities', [
+            'business_name' => 'empresa de test',
+            'cuit' => 123456789,
+        ]);
     }
 
-    // public function test_successfully_register_the_supplier()
-    // {
-    //     $entity = Entity::factory()->create();
-    //     $response = $this->postJson('api/registerClient', ['name' => 'test testing', 'business_name' => 'empresa de test', 'email' => 'creacionUser@test.com', 'cuit' => 123456789, 'password' => 'password', 'invitation_token' => $entity->invitation_token]); // ['name' => 'test testing', 'business_name' => 'empresa de test', 'email' => 'test@test.com', 'cuit' => 123456789, 'password' => "password"]);
+    public function test_successfully_register_the_supplier()
+    {
+        $entity = Entity::factory()->create();
+        $response = $this->postJson('api/registerClient', ['name' => 'test testing', 'business_name' => 'empresa de test', 'email' => 'creacionUser@test.com', 'cuit' => 123456789, 'password' => 'password', 'invitation_token' => $entity->invitation_token]); // ['name' => 'test testing', 'business_name' => 'empresa de test', 'email' => 'test@test.com', 'cuit' => 123456789, 'password' => "password"]);
 
-    //     $response->assertStatus(200)
-    //         ->assertJson(['message' => ['Client successfully registered']])
-    //         ->assertJsonStructure([
-    //             'access_token',
-    //             'token_type',
-    //             'expires_in',
-    //             'entities'
-    //         ]);
-    //     $this->assertDatabaseHas('users', [
-    //         'name' => 'test testing',
-    //         'email' => 'creacionUser@test.com',
-    //         'is_root' => 0,
-    //     ]);
+        $response->assertStatus(200)
+            ->assertJson(['message' => ['Client successfully registered']])
+            ->assertJsonStructure([
+                'access_token',
+                'token_type',
+                'expires_in',
+                'entities'
+            ]);
+        $this->assertDatabaseHas('users', [
+            'name' => 'test testing',
+            'email' => 'creacionUser@test.com',
+            'is_root' => 0,
+        ]);
 
-    //     $this->assertDatabaseHas('entities', [
-    //         'business_name' => 'empresa de test',
-    //         'cuit' => 123456789,
-    //     ]);
+        $this->assertDatabaseHas('entities', [
+            'business_name' => 'empresa de test',
+            'cuit' => 123456789,
+        ]);
 
-    //     $this->assertDatabaseHas('suppliers', [
-    //         'consumer_id' => $entity->id,
-    //     ]);
-    // }
+        $this->assertDatabaseHas('suppliers', [
+            'consumer_id' => $entity->id,
+        ]);
+    }
 }

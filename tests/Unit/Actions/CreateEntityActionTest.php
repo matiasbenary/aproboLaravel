75ea88993f9a02eb1e4f20f7cc2673f71fa61888
Matias Benary <matiasbenary@gmail.com>
2023-06-07 17:35:38 -0300

    :bug: fix error test

--- tests/Unit/Actions/CreateEntityActionTest.php
+++ tests/Unit/Actions/CreateEntityActionTest.php
@@ -4,10 +4,12 @@ namespace Tests\Unit\Actions;
 
 use App\Actions\Entity\CreateEntityAction;
 use App\Data\Entity\CreateEntityData;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CreateEntityActionTest extends TestCase
 {
+    use RefreshDatabase;
     public function test_create_entity_complete()
     {
         $entityRequest = CreateEntityData::from(['business_name' => 'test', 'fantasy_name' => 'fantasy', 'cuit' => 123456789, 'cbu' => 9876543211234, 'email' => 'test@test.com']);
